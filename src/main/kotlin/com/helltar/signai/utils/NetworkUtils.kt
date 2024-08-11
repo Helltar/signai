21e453cfafa5ce8ee91c2e035fbf0b246c68d21b
Helltar <iam@helltar.com>
2024-08-11 16:57:29 +0300

    httpGet debug log

--- src/main/kotlin/com/helltar/signai/utils/NetworkUtils.kt
+++ src/main/kotlin/com/helltar/signai/utils/NetworkUtils.kt
@@ -1,20 +1,32 @@
 package com.helltar.signai.utils
 
+import com.github.kittinunf.fuel.core.Parameters
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.httpDelete
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.fuel.httpPut
+import org.slf4j.LoggerFactory
 
 object NetworkUtils {
 
     private const val TIMEOUT = 180000
 
-    fun httpGet(url: String) =
-        url.httpGet()
-            .timeout(TIMEOUT)
-            .timeoutRead(TIMEOUT)
-            .response().second.data.decodeToString()
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    fun httpGet(url: String, parameters: Parameters = listOf()): String {
+        val response =
+            url.httpGet(parameters)
+                .timeout(TIMEOUT)
+                .timeoutRead(TIMEOUT)
+                .response().second
+
+        val json = response.data.decodeToString()
+
+        log.debug("$url --> $json")
+
+        return json
+    }
 
     fun httpPost(url: String, headers: Map<String, String> = mapOf(), jsonBody: String) =
         url.httpPost()
