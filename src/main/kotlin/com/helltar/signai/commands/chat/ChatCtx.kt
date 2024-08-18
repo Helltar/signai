11d1d0df3b3a8e567a85bd5eab5fc62274e13744
Helltar <iam@helltar.com>
2024-08-19 02:30:24 +0300

    refactor reply logic in ChatCtx
    - moved text construction logic into a private method

--- src/main/kotlin/com/helltar/signai/commands/chat/ChatCtx.kt
+++ src/main/kotlin/com/helltar/signai/commands/chat/ChatCtx.kt
@@ -7,14 +7,14 @@ import com.helltar.signai.signal.model.Receive
 class ChatCtx(envelope: Receive.Envelope) : Chat(envelope) {
 
     override fun run() {
-        val text =
-            if (userDialogHistory.isNotEmpty()) {
-                userDialogHistory
-                    .filter { it.role == CHAT_ROLE_USER }
-                    .joinToString("\n") { "- ${it.content}" }
-            } else
-                Strings.CHAT_CONTEXT_EMPTY
-
-        replyToMessage(text)
+        replyToMessage(constructUserDialogText())
     }
+
+    private fun constructUserDialogText() =
+        if (userDialogHistory.isNotEmpty()) {
+            userDialogHistory
+                .filter { it.role == CHAT_ROLE_USER }
+                .joinToString("\n") { "- ${it.content}" }
+        } else
+            Strings.CHAT_CONTEXT_EMPTY
 }
