109186a0efce1fb5c8cdcd4fa86c30691d9cde0f
Helltar <iam@helltar.com>
2024-10-19 13:35:32 +0300

    removed the gpt-4o-mini constant and updated ChatGPT.kt to use gpt-4o model instead

--- src/main/kotlin/com/helltar/signai/gpt/ChatGPT.kt
+++ src/main/kotlin/com/helltar/signai/gpt/ChatGPT.kt
@@ -2,7 +2,7 @@ package com.helltar.signai.gpt
 
 import com.helltar.signai.EnvConfig.openaiAPIKey
 import com.helltar.signai.gpt.model.Chat
-import com.helltar.signai.gpt.model.Chat.CHAT_GPT_MODEL_4_MINI
+import com.helltar.signai.gpt.model.Chat.CHAT_GPT_MODEL_4
 import com.helltar.signai.utils.NetworkUtils.httpPost
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -14,7 +14,7 @@ object ChatGPT {
     fun sendPrompt(messages: List<Chat.Message>): String {
         val url = "https://api.openai.com/v1/chat/completions"
         val headers = mapOf("Content-Type" to "application/json", "Authorization" to "Bearer $openaiAPIKey")
-        val body = json.encodeToString(Chat.Request(CHAT_GPT_MODEL_4_MINI, messages))
+        val body = json.encodeToString(Chat.Request(CHAT_GPT_MODEL_4, messages))
 
         val response = httpPost(url, headers, body)
         val responseJson = response.data.decodeToString()
--- src/main/kotlin/com/helltar/signai/gpt/model/Chat.kt
+++ src/main/kotlin/com/helltar/signai/gpt/model/Chat.kt
@@ -5,7 +5,6 @@ import kotlinx.serialization.Serializable
 object Chat {
 
     const val CHAT_GPT_MODEL_4 = "gpt-4o"
-    const val CHAT_GPT_MODEL_4_MINI = "gpt-4o-mini"
 
     const val CHAT_ROLE_USER = "user"
     const val CHAT_ROLE_ASSISTANT = "assistant"
