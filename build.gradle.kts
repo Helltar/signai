00d642bc93b0b9594b5a84bc1d6f653eae44e893
Helltar <iam@helltar.com>
2026-03-07 15:59:04 +0200

    update dependencies to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.3.0"
-    kotlin("plugin.serialization") version "2.3.0"
+    kotlin("jvm") version "2.3.10"
+    kotlin("plugin.serialization") version "2.3.10"
     id("com.gradleup.shadow") version "9.3.0"
     application
 }
@@ -13,12 +13,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-core:3.4.0")
-    implementation("io.ktor:ktor-client-cio:3.4.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.0")
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.7")
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.27")
+    implementation("io.ktor:ktor-client-core:3.4.1")
+    implementation("io.ktor:ktor-client-cio:3.4.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.1")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
+    implementation("io.github.oshai:kotlin-logging-jvm:8.0.01")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.32")
 }
 
 application {
