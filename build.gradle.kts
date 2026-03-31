c1e88a3a54f8f9b85aeb61254a4bf64e3ee5d980
Helltar <iam@helltar.com>
2026-03-31 23:05:09 +0300

    update Kotlin and ktor dependencies to latest versions and bump project version to 0.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,23 @@
 plugins {
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.serialization") version "2.3.10"
+    kotlin("jvm") version "2.3.20"
+    kotlin("plugin.serialization") version "2.3.20"
     id("com.gradleup.shadow") version "9.3.0"
     application
 }
 
 group = "com.helltar"
-version = "0.8.0"
+version = "0.9.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-core:3.4.1")
-    implementation("io.ktor:ktor-client-cio:3.4.1")
-    implementation("io.ktor:ktor-client-websockets:3.4.1")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.1")
+    implementation("io.ktor:ktor-client-core:3.4.2")
+    implementation("io.ktor:ktor-client-cio:3.4.2")
+    implementation("io.ktor:ktor-client-websockets:3.4.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.2")
+
     implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
     implementation("io.github.oshai:kotlin-logging-jvm:8.0.01")
     runtimeOnly("ch.qos.logback:logback-classic:1.5.32")
